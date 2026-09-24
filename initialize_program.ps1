5f3f1880d213c86c4b3443cebe9c77af0998164b
andrewlove12 <andrewlove33@gmail.com>
2026-09-24 16:07:55 -0400

    revert back to running python locally

--- initialize_program.ps1
+++ initialize_program.ps1
@@ -1,2 +1,4 @@
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt --no-warn-script-location
+.\python.exe -m venv venv
+.\venv\Scripts\Activate.ps1
+.\python.exe -m pip install --upgrade pip
+.\python.exe -m pip install -r requirements.txt --no-warn-script-location
